2c69a550fa076230d18b791d9e485439e27b92be
Tatsumasa Ishikawa <tatsumasa.ishikawa@ishsoftware.jp>
2023-07-31 22:10:03 +0900

    change default lang to ja

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,8 +28,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'ja',
+    locales: ['en', 'ja'],
   },
 
   presets: [
@@ -72,6 +72,10 @@ const config = {
             href: 'https://github.com/F3DS-Package/F3DS-Package',
             label: 'GitHub',
             position: 'right',
+          },
+          {
+            type: 'localeDropdown',
+            position: 'left',
           },
         ],
       },
